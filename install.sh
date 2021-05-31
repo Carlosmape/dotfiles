d769770b80d2ce7ed4bf0ed1553f237634c1f39d
Carlosmape <cagrmape@gmail.com>
2021-05-31 23:06:20 +0200

    Added dummy install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Carlosmape environment installation process"
+echo "This script will install needed packages and will deploy entire setup configuration files"
+echo "Press ENTER to CONTINUE"
+echo "Press CTRL + C to CANCEL"
+read
+echo "TODO: Installing packages"
+echo "TODO: Deploy environment settings"
