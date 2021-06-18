48819c8d6167b8e684ad11a23f3a6cbe17417a83
Carlosmape <cagrmape@gmail.com>
2021-06-18 20:27:18 +0200

    Added install dependency brightnessctl

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Press ENTER to CONTINUE"
 echo "Press CTRL + C to CANCEL"
 read
 echo "TODO: Installing packages"
-pacman -S bspwm polybar picom feh
+pacman -S bspwm polybar picom feh brightnessctl
 echo "TODO: Deploy environment settings"
 cp -rvf confFiles/* ~/.config/
 echo "# - Fetching background repository"
