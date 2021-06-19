ae1f9f172997bd107d9214011b22f3264ab53c94
Carlosmape <cagrmape@gmail.com>
2021-06-19 13:08:03 +0200

    Solved issue with icon-font 'siji'. Now it install from github repo

--- install.sh
+++ install.sh
@@ -6,8 +6,16 @@ echo "Press ENTER to CONTINUE"
 echo "Press CTRL + C to CANCEL"
 read
 echo "TODO: Installing packages"
-pacman -S bspwm polybar picom feh brightnessctl dmenu urxvt scrot amixer
-echo "TODO: Deploy environment settings"
-cp -rvf confFiles/* ~/.config/
+sudo pacman -S bspwm polybar picom feh brightnessctl dmenu urxvt scrot amixer xorg-xfd
 echo "# - Fetching background repository"
 git clone https://gist.github.com/85942af486eb79118467.git ~/Pictures/wallpapers
+echo "# - Fetching siji bitmap font repository"
+git clone https://github.com/stark/siji
+sudo ./siji/install.sh -d /usr/share/fonts/siji/
+#sudo xset +fp /usr/share/fonts/siji/
+#sudo xset rehash && xset rehash
+
+echo "TODO: Deploy environment settings"
+echo "# - Deploying dotfiles"
+cp -rvf confFiles/* ~/.config/
+
